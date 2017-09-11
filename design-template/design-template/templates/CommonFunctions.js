3a675666bb278daad29958d572d1747b073038fb
Navneet <navneetc92@gmail.com>
2017-09-11 10:29:57 +0530

    Design template

---
+++ design-template/design-template/templates/CommonFunctions.js
@@ -0,0 +1,87 @@
+﻿var GlobalMessages = {
+    FormOrganizationValidationFailedMessage: "Please fill required fields with valid inputs!"
+};
+
+$(document).ready(function () {
+    //$(".datepicker").datepicker();
+    //********* Ajax global events **********
+    CommonUtility.InitiateAjaxHandler();
+    if (CommonUtility.GetCookieByName("ActiveSideMenuOrder") == null)
+        CommonUtility.SetActiveMenuCookie("1");
+
+    $.each($('#side-menu a'), function () {
+        if ($(this).attr('order') == CommonUtility.GetCookieByName("ActiveSideMenuOrder")) {
+            $(this).addClass('active');
+            if ($(this).parent().closest('ul').attr('aria-expanded') == 'false') {
+                $(this).parent().closest('ul').attr('aria-expanded', 'true');
+                $(this).parent().closest('ul').addClass('in');
+            }
+        }
+    });
+});
+
+//$(document).ajaxError(function (event, jqxhr, settings, exception) {
+//    if (jqxhr.status === 1) {
+//        location.reload();
+//    }
+//});
+
+var CommonUtility = {
+    InitiateAjaxHandler: function () {
+        $(document).ajaxStart(function () {
+            $("#loader-Container").show();
+        }).ajaxStop(function () {
+            $("#loader-Container").hide();
+        });
+    },
+
+    CallAjaxService: function (method, serviceUrl, data, successCallback, errorCallback) {
+        $.ajax({
+            type: method,
+            url: serviceUrl,
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            dataType: 'json',
+            success: successCallback,
+            error: errorCallback
+        });
+    },
+
+    CallAjaxServiceFileUpload: function (method, serviceUrl, data, successCallback, errorCallback) {
+        $.ajax({
+            type: method,
+            url: serviceUrl,
+            data: data,
+            contentType: false,
+            dataType: 'json',
+            processData: false,
+            success: successCallback,
+            error: errorCallback
+        });
+    },
+
+    ShowStatus: function (ref, message) {       
+        $(ref).html(message);
+        $(ref).parent().removeClass('hidden');
+        $(ref).parent().fadeIn(500);
+        $(ref).parent().fadeOut(10000);       
+    },
+
+    GetCookieByName: function (name) {
+        var re = new RegExp(name + "=([^;]+)");
+        var value = re.exec(document.cookie);
+        return (value != null) ? unescape(value[1]) : null;
+    },
+
+    SetActiveMenuCookie: function (order) {
+        var mins = 30;
+        var date = new Date();
+        date.setTime(date.getTime() + (mins * 60 * 1000));
+        document.cookie = "ActiveSideMenuOrder=" + order + ";expires=" + date.toGMTString() + ";path=/";
+    },
+
+    ClearCookieOnLogOut: function (logOutAction) {
+        document.cookie = "ActiveSideMenuOrder=;expires=Thu,01 Jan 1970 00:00:00 UTC;path=/;";
+        window.location.href = logOutAction;
+    }
+};
---
+++ design-template/design-template/templates/InputValidation.js
@@ -0,0 +1,90 @@
+﻿$(function () {
+    
+    $("input.required[type=text],textarea.required,input.required[type=password]").blur(function () {
+        if (($(this).val().trim() == "" || $(this).val() == null)) {
+            $(this).closest('.form-group').addClass('has-error');
+        }
+        else {
+            $(this).closest('.form-group').removeClass('has-error');
+        }
+    });
+
+    $(".password,.confirmPassword").blur(function () {
+        if ($(".password").val() != $(".confirmPassword").val() || $(".password").val() == '') {
+            $(".password,.confirmPassword").closest('.form-group').addClass('has-error');
+        }
+        else {
+            $(".password,.confirmPassword").closest('.form-group').removeClass('has-error');
+        }
+    });
+
+    $("select.required").blur(function () {
+        if (($(this).prop('selectedIndex') == 0)) {
+            $(this).closest('.form-group').addClass('has-error');
+        }
+        else {
+            $(this).closest('.form-group').removeClass('has-error');
+        }
+    });
+
+    $(".emailAddress").blur(function () {
+        var expression = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+        if (!expression.test($(this).val())) {
+            $(this).closest('.form-group').addClass('has-error');
+        }
+        else {
+            $(this).closest('.form-group').removeClass('has-error');
+        }
+    });
+});
+
+
+var InputValidation = {
+
+    IsFormValid: function (context) {
+        //// For required input box controls
+        $("input.required[type=text],textarea.required,input.required[type=password]", $(context)).each(function () {
+            if (($(this).val().trim() == "" || $(this).val() == null)) {
+                $(this).closest('.form-group').addClass('has-error');
+            }
+            else {
+                $(this).closest('.form-group').removeClass('has-error');
+            }
+        });
+
+        $("select.required", $(context)).each(function () {
+            if (($(this).prop('selectedIndex') == 0)) {
+                $(this).closest('.form-group').addClass('has-error');
+            }
+            else {
+                $(this).closest('.form-group').removeClass('has-error');
+            }
+        });
+
+        if ($(".password", $(context)).length == 1) {
+            if ($(".password", $(context)).val() != $(".confirmPassword", $(context)).val() || $(".password", $(context)).val() == '') {
+                $(".password,.confirmPassword", $(context)).closest('.form-group').addClass('has-error');
+            }
+            else {
+                $(".password,.confirmPassword", $(context)).closest('.form-group').removeClass('has-error');
+            }
+        }
+
+        $(".emailAddress", $(context)).each(function () {
+            var expression = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+            if (!expression.test($(this).val())) {
+                $(this).closest('.form-group').addClass('has-error');
+            }
+            else {
+                $(this).closest('.form-group').removeClass('has-error');
+            }
+        });
+
+        if ($('.has-error', $(context)).length > 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
